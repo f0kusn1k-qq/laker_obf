c8f57b7d99cc140243bec36a35f62695d017b84d
Serpensin <63426826+Serpensin@users.noreply.github.com>
2025-04-07 16:29:43 +0200

    Update watermark.lua

--- Hercules/watermark.lua
+++ Hercules/watermark.lua
@@ -2,7 +2,7 @@
 local Watermark = {}
 
 function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6.2 -> https://github.com/zeusssz/hercules-obfuscator | by using the Hercules bot -> https://top.gg/bot/1293608330123804682]\n" .. code
+    return "--[Obfuscated by Hercules v1.6.2 -> https://github.com/zeusssz/hercules-obfuscator | zeusssz.github.io/hercules-discord/ | by using the Hercules bot -> https://top.gg/bot/1293608330123804682]\n" .. code
 end
 
 return Watermark
