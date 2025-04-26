900c7fa50a2a32eb456025e166623242716f1382
f0kusn1k <95432429+f0kusn1k-qq@users.noreply.github.com>
2025-04-26 10:00:06 +0300

    watermark update

--- Hercules/watermark.lua
+++ Hercules/watermark.lua
@@ -1,8 +1,19 @@
 -- modules/watermark.lua
-local Watermark = {}
-
 function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6.2 -> https://github.com/zeusssz/hercules-obfuscator | zeusssz.github.io/hercules-discord/ | by using the Hercules bot -> https://top.gg/bot/1293608330123804682]\n" .. code
+    local watermark = [[
+         __    ____                      __           __   __                 __      __      ____      _ __  
+  ____  / /_  / __/_  ________________ _/ /____  ____/ /  / /_  __  __   ____/ /___  / /___  / __/___  (_) /__
+ / __ \/ __ \/ /_/ / / / ___/ ___/ __ `/ __/ _ \/ __  /  / __ \/ / / /  / __  / __ \/ / __ \/ /_/ __ \/ / //_/
+/ /_/ / /_/ / __/ /_/ (__  ) /__/ /_/ / /_/  __/ /_/ /  / /_/ / /_/ /  / /_/ / /_/ / / /_/ / __/ / / / / ,<   
+\____/_.___/_/  \__,_/____/\___/\__,_/\__/\___/\__,_/  /_.___/\__, /   \__,_/\____/_/\____/_/ /_/ /_/_/_/|_|  
+                                                             /____/      
+                                        start from loader instad it may crash u xD
+]]
+    local result = ""
+    for line in watermark:gmatch("[^\r\n]+") do
+        result = result .. "--" .. line .. "\n"
+    end
+    return result .. code
 end
 
 return Watermark
